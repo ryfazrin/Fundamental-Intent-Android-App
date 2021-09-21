e02c5b695f7fdffdf366af8c43d80b9adf788617
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-21 11:34:02 +0800

    Update MoveForResultActivity
    add component to MoveForResultActivity

--- app/src/main/java/com/dicoding/myintentapp/MoveForResultActivity.kt
+++ app/src/main/java/com/dicoding/myintentapp/MoveForResultActivity.kt
@@ -2,10 +2,24 @@ package com.dicoding.myintentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
 
 class MoveForResultActivity : AppCompatActivity() {
+
+    private lateinit var btnChoose: Button
+    private lateinit var rgNumber: RadioGroup
+
+    companion object {
+        const val EXTRA_SELECTED_VALUE = "extra_selectede_value"
+        const val RESULT_CODE = 110
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_for_result)
+
+        btnChoose = findViewById(R.id.btn_choose)
+        rgNumber = findViewById(R.id.rg_number)
     }
 }
