479b63f0dac63e264fbf1daab004a3cf191ececb
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-21 09:13:32 +0800

    add MoveWithObjectActivity
    MoveWithObejctActivity

---
+++ app/src/main/java/com/dicoding/myintentapp/MoveWithObjectActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveWithObjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_object)
+    }
+}
